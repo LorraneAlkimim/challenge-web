67a6bf76900d8347e18c8eb516a9f560eca0b2d9
LorraneAlkimim <lorranealkmim@yahoo.com.br>
2023-11-08 22:02:59 -0300

    feat: setup react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
+import { Route, Routes } from "react-router-dom"
+import { Sales } from "./pages/Sales"
+
 function App() {
 
   return (
-    <h1>Hello World!</h1>
+    <Routes>
+      <Route path="/" element={<Sales />} />
+    </Routes>
   )
 }
 
