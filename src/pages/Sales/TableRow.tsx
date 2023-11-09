b484a2ecd36eb3d5a033c6d141f86aa6e2974c06
LorraneAlkimim <lorranealkmim@yahoo.com.br>
2023-11-09 00:20:34 -0300

    feat: creates sales page

---
+++ src/pages/Sales/TableRow.tsx
@@ -0,0 +1,76 @@
+import { useState } from 'react'
+import { FaEdit, FaTrash } from 'react-icons/fa'
+
+import styles from './styles.module.scss'
+
+export function TableRow() {
+  const [isOpened, setIsOpened] = useState(false)
+
+  return (
+    <>
+      <tr>
+        <td>003</td>
+        <td>Customer Name</td>
+        <td>Seller Name</td>
+        <td>09/11/2022</td>
+        <td>R$ 99,75</td>
+        <td>
+          <div className={styles.actions}>
+            <button type='button' onClick={() => setIsOpened(prev => !prev)}>{isOpened ? 'Fechar' : 'Ver itens'}</button>
+
+            <button onClick={() => {}} type='button'>
+              <FaEdit size={16} />
+            </button>
+
+            <button onClick={() => {}}>
+              <FaTrash size={16} />
+            </button>
+          </div>
+        </td>
+      </tr>
+
+      {isOpened ? (
+        <tr>
+          <td colSpan={6}>
+            <table className={styles['is-expanded']}>
+              <thead>
+                <tr>
+                  <th>Produto/Serviço</th>
+                  <th>Quantidade</th>
+                  <th>Preço unitário</th>
+                  <th>Total do Produto</th>
+                  <th>% de Comissão</th>
+                  <th>Comissão</th>
+                </tr>
+              </thead>
+
+              <tbody>
+                {Array.from({length: 4}).map((_, index) => (
+                  <tr key={index}>
+                    <td>002 - Product name</td>
+                    <td>0</td>
+                    <td>R$ 31,00</td>
+                    <td>R$ 03,99</td>
+                    <td>7%</td>
+                    <td>R$ 3,67</td>
+                  </tr>
+                ))}
+              </tbody>
+
+              <tfoot>
+                <tr>
+                  <td>Total da Venda</td>
+                  <td>7</td>
+                  <td></td>
+                  <td>R$ 39,90</td>
+                  <td></td>
+                  <td>R$ 3,50</td>
+                </tr>
+              </tfoot>
+            </table>
+          </td>
+        </tr>
+      ) : null}
+    </>
+  )
+}
--- src/pages/Sales/index.tsx
+++ src/pages/Sales/index.tsx
@@ -1,5 +1,44 @@
+import { Header } from '../../components/Header';
+import { Link } from '../../components/Link';
+import { TableRow } from './TableRow';
+
+import styles from './styles.module.scss';
+
 export function Sales() {
+
   return (
-    <h1>Sales</h1>
+    <>
+      <Header title='Vendas' />
+
+      <main className={styles.container}>
+        <div className={styles['title-wrapper']}>
+          <h2 className={styles.title}>Vendas Realizadas</h2>
+
+          <Link asButton>Inserir nova Venda</Link>
+        </div>
+
+        <div className={styles['table-wrapper']}>
+          <table>
+            <thead>
+              <tr>
+                <th>Nota Fiscal</th>
+                <th>Cliente</th>
+                <th>Vendedor</th>
+                <th>Data de Venda</th>
+                <th>Valor Total</th>
+                <th>Opções</th>
+              </tr>
+            </thead>
+            <tbody>
+              {Array.from({length: 4}).map((_, index) => (
+                <TableRow
+                  key={index}
+                />
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </main>
+    </>
   )
 }
